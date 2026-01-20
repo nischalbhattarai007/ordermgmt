c340629be3322db939b8e28c9cd64e8b493b3667
Nischal Bhattarai <125273286+nischalbhattarai007@users.noreply.github.com>
2026-01-20 13:23:15 +0545

    Delete ordermgmt/ordermgmt/src directory

--- ordermgmt/ordermgmt/src/main/java/com/example/ordermgmt/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ordermgmt.repository;
-
-import com.example.ordermgmt.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Integer> {
-}
--- ordermgmt/ordermgmt/src/main/java/com/example/ordermgmt/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ordermgmt.repository;
-
-import com.example.ordermgmt.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
